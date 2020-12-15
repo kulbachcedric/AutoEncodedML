20848a2a2560ab672675d4b64e82fc74f5e425b8
Lucas <lucas.cazzonelli@gmail.com>
2020-12-15 11:58:05 +0100

    -Updated AE class

--- experiments/test.py
+++ experiments/test.py
@@ -18,10 +18,16 @@ import numpy as np
 dataset_ids = [40996, 40668, 1492, 44]
 
 layer_sizes1 = [(1.0, 0.05), (1.0, 0.1), (1.0, 0.2), (1.0, 0.4), (1.0, 0.8)]
+
 layer_sizes2 = [(0.1, 0.1), (0.2, 0.2), (0.4, 0.4), (0.8, 0.8), (1.6, 1.6)]
+
 layer_sizes3 = [(0.1, 0.1), (0.2, 0.1), (0.4, 0.1), (0.8, 0.1), (1.6, 0.1)]
+
 layer_sizes = [(1.0, 0.05), (1.0, 0.1), (1.0, 0.2), (1.0, 0.4), (1.0, 0.8),
                (0.1, 0.1), (0.2, 0.1), (0.4, 0.1), (0.8, 0.1), (1.6, 0.1)]
+
+layers = [(1, 2, 3, 4, 5)]
+activations = ['selu', 'relu', 'tanh', 'sigmoid']
 
 
 def test_dataset(dataset_id):
@@ -49,6 +55,26 @@ def test_dataset(dataset_id):
     return cv_results_to_df(grid.cv_results_)
 
 
+def calculate_hidden_dims(net_shape, hidden_dims, n_layers, input_shape):
+
+    dim_calculator = np.linspace if net_shape == 'linear' else np.geomspace
+
+    if isinstance(hidden_dims, float):
+        dims = hidden_dims * np.prod(input_shape)
+    elif isinstance(hidden_dims, (tuple, list)):
+        dims = [dim * np.prod(input_shape) if isinstance(dim, float) else dim for dim in hidden_dims]
+    else:
+        dims = hidden_dims
+
+    if isinstance(dims, (tuple, list)) and len(dims) == 2:
+        dims = dim_calculator(*dims, num=n_layers)
+    elif isinstance(dims, (int, float)):
+        dims = dim_calculator(np.prod(input_shape), dims, num=n_layers + 1)[1:]
+
+    dims = np.round(dims).astype(int)
+    return tuple(dims)
+
+
 if __name__ == '__main__':
     results = {}
     for dataset_id in dataset_ids:
@@ -59,3 +85,4 @@ if __name__ == '__main__':
     df.index.names = ('dataset_id', 'idx')
     df.reset_index(level='idx', drop=True, inplace=True)
     df.to_csv('gridsearch_results.csv')
+
