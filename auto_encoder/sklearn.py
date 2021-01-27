9fd5bd44d4db7677a9f27630d5ff8bb78e6f2d64
Lucas <lucas.cazzonelli@yahoo.de>
2021-01-27 16:16:50 +0100

    -fixed parameter assignments

--- auto_encoder/sklearn.py
+++ auto_encoder/sklearn.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-import tensorflow as tf
+
 from auto_encoder.model import *
 
 
@@ -48,27 +48,58 @@ class Transformer(BaseEstimator, TransformerMixin):
 class AutoTransformer(Transformer):
     def __init__(self, type='ae', loss='binary_crossentropy', optimizer='adam', max_epochs=100, hidden_dims=0.2,
                  n_layers=3, activation='selu', output_activation='sigmoid'):
-        self.type = type
-        self.loss = loss
-        self.optimizer = optimizer
-        self.max_epochs = max_epochs
+        super(AutoTransformer, self).__init__(type, loss, optimizer, max_epochs)
         self.hidden_dims = hidden_dims
         self.n_layers = n_layers
         self.activation = activation
         self.output_activation = output_activation
-        super(AutoTransformer, self).__init__(type=self.type, loss=self.loss, optimizer=self.optimizer,
-                                              max_epochs=self.max_epochs, hidden_dims=self.hidden_dims,
-                                              n_layers=self.n_layers, activation=self.activation,
-                                              output_activation=self.output_activation)
+
+    def fit(self, X, y=None):
+        self.n_features_ = X.shape[1] if len(X.shape) == 2 else X.shape[1:]
+
+        if isinstance(self.type, tf.keras.Model):
+            self.model = self.type
+        else:
+            self.model = Transformer.models[self.type](self.hidden_dims, self.n_layers, self.activation,
+                                                       self.output_activation)
+        self.model.compile(loss=self.loss, optimizer=self.optimizer)
+        self.model.fit(X, X, epochs=self.max_epochs, validation_split=0.1, callbacks=[self.callback])
+        tf.keras.backend.clear_session()
+        return self
 
 
 class ConvolutionalAutoTransformer(Transformer):
     def __init__(self, type='cae', loss='binary_crossentropy', optimizer='adam', max_epochs=100, filters_base=8,
                  pooling=2, k_size=3, n_conv=2, strides=1, activation='selu', latent_dim=0.2,
                  output_activation='sigmoid'):
-        super().__init__(type=type, loss=loss, optimizer=optimizer, max_epochs=max_epochs, filters_base=filters_base,
-                         pooling=pooling, k_size=k_size, n_conv=n_conv, strides=strides, activation=activation,
-                         latent_dim=latent_dim, output_activation=output_activation)
+        super().__init__(type=type, loss=loss, optimizer=optimizer, max_epochs=max_epochs)
+        self.filters_base = filters_base
+        self.pooling = pooling
+        self.k_size = k_size
+        self.n_conv = n_conv
+        self.strides = strides
+        self.activation = activation
+        self.latent_dim = latent_dim
+        self.output_activation = output_activation
+
+    def fit(self, X, y=None):
+        self.n_features_ = X.shape[1] if len(X.shape) == 2 else X.shape[1:]
+
+        if isinstance(self.type, tf.keras.Model):
+            self.model = self.type
+        else:
+            self.model = Transformer.models[self.type](self.filters_base,
+                                                       self.pooling,
+                                                       self.k_size,
+                                                       self.n_conv,
+                                                       self.strides,
+                                                       self.activation,
+                                                       self.latent_dim,
+                                                       self.output_activation)
+        self.model.compile(loss=self.loss, optimizer=self.optimizer)
+        self.model.fit(X, X, epochs=self.max_epochs, validation_split=0.1, callbacks=[self.callback])
+        tf.keras.backend.clear_session()
+        return self
 
 
 class IdentityTransformer(BaseEstimator, TransformerMixin):
